aa559cebb05dd66f9e84ead5e30875d272d73142
falskuyyy <ahmdfalah10@gmail.com>
2023-04-03 11:17:30 +0700

    membangun struktur Alamatdetail

--- activitystructure2_148/activitystructure2_148.cpp
+++ activitystructure2_148/activitystructure2_148.cpp
@@ -7,33 +7,3 @@ struct AlamatDetail
 	string kota;
 };
 
-struct Mahasiswa
-{
-	string NIM;
-	string nama;
-	AlamatDetail alamat;
-	int umur;
-};
-
-
-int main() {
-	Mahasiswa mhs;
-
-	cout << "Masukkan NIM :";
-	cin >> mhs.NIM;
-	cout << "Masukkan Nama :";
-	cin >> mhs.nama;
-	cout << "Masukkan Alamat :";
-	cout << "\n\tMasukkan Desa :";
-	cin >> mhs.alamat.desa;
-	cout << "\tMasukkan Kota :";
-	cin >> mhs.alamat.kota;
-	cout << "Masukkan Umur :";
-	cin >> mhs.umur;
-
-	cout << "\n\nNIM = " << mhs.NIM;
-	cout << "\nNama = " << mhs.nama;
-	cout << "\nDesa = " << mhs.alamat.desa;
-	cout << "\nkota = " << mhs.alamat.kota;
-	cout << "\nUmur = " << mhs.umur;
-}
